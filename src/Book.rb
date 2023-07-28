95ae2fda5f28df133a557aa8377005518f4ab13b
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-28 15:43:33 +0330

    Add Book class with title and author properties.

---
+++ src/Book.rb
@@ -0,0 +1,10 @@
+class Book
+    attr_accessor :title, :author, :rentals
+  
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @rentals = []
+    end
+  end
+  
