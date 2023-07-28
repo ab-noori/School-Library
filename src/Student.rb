b48d15cb4d443997675c8d9218ee3998ebde2d7f
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-28 15:52:07 +0330

    Implement has-many/belongs-to relationship between Classroom and Student.

---
+++ src/Student.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(name = "Unknown", age = 0, parent_permission = true, classroom = nil)
+    super(name, age, parent_permission)
+    @classroom = classroom
+    classroom.add_student(self) if classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
